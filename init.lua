dd3c718c3150bf17cffa8ffe9a5d32c09cd79939
I749983 <lkronenb1@googlemail.com>
2025-06-07 12:26:23 +0200

    session management

--- init.lua
+++ init.lua
@@ -19,6 +19,18 @@ require("options")
 
 require("lazy").setup({
   {
+  	'rmagatti/auto-session',
+  	lazy = false,
+
+  	---enables autocomplete for opts
+  	---@module "auto-session"
+  	---@type AutoSession.Config
+  	opts = {
+   	 suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+    	-- log_level = 'debug',
+  	}
+  },
+  {
     "brianhuster/autosave.nvim",
     lazy=false,
     event="InsertEnter",
